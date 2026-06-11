a1ceb52ac566b315128a3aad58408371a213cd40
SergioRdzMndz <utm24090656@utma.edu.mx>
2026-06-10 22:38:58 -0600

    feat: Backend API initial structure

---
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/TravelAgencyManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.utma.tams.travel_agency_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyManagementSystemApiApplication.class, args);
+	}
+
+}
