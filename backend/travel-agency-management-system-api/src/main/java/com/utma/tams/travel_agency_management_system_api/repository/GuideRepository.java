ea2fa9b3d10d456dd049558a28eba6dc28c5e04c
Sergio <sergiorod210706@gmail.com>
2026-06-14 23:01:22 -0600

    feat: Add GuideRepository for database operations

---
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/GuideRepository.java
@@ -0,0 +1,11 @@
+package com.utma.tams.travel_agency_management_system_api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.utma.tams.travel_agency_management_system_api.model.Guide;
+
+public interface GuideRepository extends JpaRepository<Guide, Long> {
+    Optional<Guide> findById(Long id);
+}
