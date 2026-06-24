e7bc1d679c4d1f1541b6c257b1a2b51a2b83001d
Sergio <sergiorod210706@gmail.com>
2026-06-24 00:56:00 -0600

    feat: CardRepository created

---
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/CardRepository.java
@@ -0,0 +1,11 @@
+package com.utma.tams.travel_agency_management_system_api.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.utma.tams.travel_agency_management_system_api.model.Card;
+
+public interface CardRepository extends JpaRepository<Card,Long> {
+    
+}
