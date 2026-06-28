42d3bfc4d92cf44013f1629a4bf6074f30673869
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:36:42 -0600

    feat: Methods added to GuideRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/GuideRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/GuideRepository.java
@@ -1,5 +1,6 @@
 package com.utma.tams.travel_agency_management_system_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import com.utma.tams.travel_agency_management_system_api.model.Guide;
 
 public interface GuideRepository extends JpaRepository<Guide, Long> {
     Optional<Guide> findById(Long id);
+    List<Guide> findByAvailable(Boolean available);
 }
