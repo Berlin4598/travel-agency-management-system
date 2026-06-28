925cef60181c435722035ef4fdef8d1de4faea79
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:38:33 -0600

    feat: Methods added to UserRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/UserRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.utma.tams.travel_agency_management_system_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import com.utma.tams.travel_agency_management_system_api.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
+    List<User> findByRole(String role);
 }
