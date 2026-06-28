cab6ff5522a21b47e95c0e1626bbfaa40890e047
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:36:19 -0600

    feat: Methods added to DestinationRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/DestinationRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/DestinationRepository.java
@@ -1,5 +1,6 @@
 package com.utma.tams.travel_agency_management_system_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,7 @@ import com.utma.tams.travel_agency_management_system_api.model.Destination;
 
 public interface DestinationRepository extends JpaRepository<Destination, Long>{
     Optional<Destination> findById(Long id);
-
+    List<Destination> findByCountry(String country);
+    List<Destination> findByCity(String city);
+    
 }
