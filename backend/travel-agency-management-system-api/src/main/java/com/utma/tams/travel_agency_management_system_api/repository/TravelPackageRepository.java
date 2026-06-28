71912fe6c27945bc2186e2f68cf1f164cd251365
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:37:59 -0600

    feat: Methods added to TravelPackageRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/TravelPackageRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/TravelPackageRepository.java
@@ -1,5 +1,6 @@
 package com.utma.tams.travel_agency_management_system_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,8 @@ import com.utma.tams.travel_agency_management_system_api.model.TravelPackage;
 
 public interface TravelPackageRepository extends JpaRepository<TravelPackage, Long> {
     Optional<TravelPackage> findById(Long id);
+    List<TravelPackage> findByDestinationId(Long destinationId);
+    List<TravelPackage> findByTransportationId(Long transportationId);
+
 
 }
