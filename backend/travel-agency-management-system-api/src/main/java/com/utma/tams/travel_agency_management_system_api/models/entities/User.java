5717b10ca969d181c1b346c7431b2385c73ff385
Sergio <sergiorod210706@gmail.com>
2026-06-28 00:30:47 -0600

    feat: getters and setters addes

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/models/entities/User.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/models/entities/User.java
@@ -116,6 +116,15 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+    
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getAddress() {
         return address;
