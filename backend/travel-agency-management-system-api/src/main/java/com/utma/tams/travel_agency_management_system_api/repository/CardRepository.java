2350f5d07d8f249cf3664954916e870924488a39
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:36:00 -0600

    feat: Methods added to CardRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/CardRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/CardRepository.java
@@ -2,10 +2,12 @@ package com.utma.tams.travel_agency_management_system_api.repository;
 
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.utma.tams.travel_agency_management_system_api.model.Card;
 
 public interface CardRepository extends JpaRepository<Card,Long> {
-    
+    Optional<Card> findByUserId(Long userId);
 }
