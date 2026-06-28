67e4284d5d26150b875f94a5100263a7d402254a
Sergio <sergiorod210706@gmail.com>
2026-06-27 18:37:15 -0600

    feat: Methods added to ReservationRepository

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/ReservationRepository.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.utma.tams.travel_agency_management_system_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,8 @@ import com.utma.tams.travel_agency_management_system_api.model.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findById(Long id);
+    List<Reservation> findByUserId(Long userId);
+    List<Reservation> findByTravelPackageId(Long packageId);
+    List<Reservation> findByReservationStatus(String status); 
+    List<Reservation> findByGuideId(Long guideId);
 }
