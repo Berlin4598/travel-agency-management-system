d9036727de4439c213784bd3fff54d4c5701a93f
SergioRdzMndz <utm24090656@utma.edu.mx>
2026-07-06 10:53:46 -0600

    feat: Card Service added

---
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/services/CardService.java
@@ -0,0 +1,21 @@
+package com.utma.tams.travel_agency_management_system_api.services;
+
+import org.springframework.stereotype.Service;
+
+// import com.utma.tams.travel_agency_management_system_api.models.dto.response.CardResponseDTO;
+// import com.utma.tams.travel_agency_management_system_api.models.entities.Card;
+// import com.utma.tams.travel_agency_management_system_api.repository.CardRepository;
+
+@Service
+public class CardService {
+
+    // private final CardRepository cardRepository;
+
+    // public CardService(CardRepository cardRepository){
+    //     this.cardRepository = cardRepository;
+    // }
+
+    // private CardResponseDTO toResponseDTO(Card card){
+    //     return new CardResponseDTO(card.getAccountNumber());
+    // }
+}
