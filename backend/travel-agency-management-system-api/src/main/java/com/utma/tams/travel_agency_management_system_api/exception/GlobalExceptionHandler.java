6f3ad56eb42d583f115fc1f9fc67f361961b720b
Sergio <sergiorod210706@gmail.com>
2026-07-06 21:09:10 -0600

    feat: Missing params handler exception

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/exception/GlobalExceptionHandler.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,11 +14,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>>handleValidationException(MethodArgumentNotValidException ex){
-        Map<String, String> errors = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();   
         ex.getBindingResult().getFieldErrors().forEach(error ->
             errors.put(error.getField(), error.getDefaultMessage())
         );
         
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String,String>> handleMissingParams(MissingServletRequestParameterException ex){
+        Map<String, String> error = new HashMap<>();
+        error.put("message: ", "Parameter '" + ex.getParameterName() + "' is missing");
+        
+        return ResponseEntity.badRequest().body(error);
     }
 }
