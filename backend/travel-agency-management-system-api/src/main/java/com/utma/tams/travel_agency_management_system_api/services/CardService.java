279c0e49d03b0a58fcdc9222536b36597926c216
Sergio <sergiorod210706@gmail.com>
2026-07-06 20:30:48 -0600

    feat: Card service created with CRUD methods

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/services/CardService.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/services/CardService.java
@@ -1,21 +1,68 @@
 package com.utma.tams.travel_agency_management_system_api.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
-// import com.utma.tams.travel_agency_management_system_api.models.dto.response.CardResponseDTO;
-// import com.utma.tams.travel_agency_management_system_api.models.entities.Card;
-// import com.utma.tams.travel_agency_management_system_api.repository.CardRepository;
+import com.utma.tams.travel_agency_management_system_api.models.dto.request.CardRequestDTO;
+import com.utma.tams.travel_agency_management_system_api.models.dto.response.CardResponseDTO;
+import com.utma.tams.travel_agency_management_system_api.models.entities.Card;
+import com.utma.tams.travel_agency_management_system_api.models.entities.User;
+import com.utma.tams.travel_agency_management_system_api.repository.CardRepository;
+import com.utma.tams.travel_agency_management_system_api.repository.UserRepository;
 
 @Service
 public class CardService {
 
-    // private final CardRepository cardRepository;
+    private final CardRepository cardRepository;
+    private final UserRepository userRepository;
 
-    // public CardService(CardRepository cardRepository){
-    //     this.cardRepository = cardRepository;
-    // }
+    public CardService(CardRepository cardRepository, UserRepository userRepository){
+        this.cardRepository = cardRepository;
+        this.userRepository = userRepository;
+    }
 
-    // private CardResponseDTO toResponseDTO(Card card){
-    //     return new CardResponseDTO(card.getAccountNumber());
-    // }
+    private CardResponseDTO toResponseDTO(Card card){
+        return new CardResponseDTO(card.getId(), card.getOwnerName(), card.getAccountNumber(), card.getExpirationDate(), card.getUser().getId());
+    }
+
+    private Card findById(Long id){
+        return cardRepository.findById(id).orElseThrow(()-> new RuntimeException("Card not found"));
+    }
+
+    public List<CardResponseDTO> getAllCards(){
+        return cardRepository.findAll().stream().map(this::toResponseDTO).collect(Collectors.toList());
+    }
+
+    public CardResponseDTO getCardById(Long id){
+        return toResponseDTO(findById(id));
+    }
+
+    public CardResponseDTO createCard(CardRequestDTO requestDTO, Long userId){
+        User user = userRepository.findById(userId).orElseThrow(()-> new RuntimeException("User not found"));
+
+        Card card = new Card(requestDTO.getOwnerName(), requestDTO.getAccountNumber(), requestDTO.getExpirationDate(), requestDTO.getCvv(), user);
+
+        cardRepository.save(card);
+        return toResponseDTO(card);
+    }
+
+    public CardResponseDTO updateCard(Long cardId, CardRequestDTO requestDTO){
+        Card card = findById(cardId);
+        
+        card.setOwnerName(requestDTO.getOwnerName());
+        card.setAccountNumber(requestDTO.getAccountNumber());
+        card.setExpirationDate(requestDTO.getExpirationDate());
+        card.setCvv(requestDTO.getCvv());
+
+        cardRepository.save(card);
+        return toResponseDTO(card);
+    }
+
+    public void deleteCard(Long cardId){
+        cardRepository.delete(findById(cardId));
+    }
+
+
 }
