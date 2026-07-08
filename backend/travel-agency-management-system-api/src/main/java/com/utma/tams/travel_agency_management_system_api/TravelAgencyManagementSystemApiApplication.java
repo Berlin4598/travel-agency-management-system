292f200f5679ef3cc5ddd22d4022c6fe2e186c92
SergioRdzMndz <utm24090656@utma.edu.mx>
2026-07-08 11:04:00 -0600

    feat: Implement weather API

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/TravelAgencyManagementSystemApiApplication.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/TravelAgencyManagementSystemApiApplication.java
@@ -2,12 +2,19 @@ package com.utma.tams.travel_agency_management_system_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TravelAgencyManagementSystemApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelAgencyManagementSystemApiApplication.class, args);
+	}
+
+	@Bean 
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
