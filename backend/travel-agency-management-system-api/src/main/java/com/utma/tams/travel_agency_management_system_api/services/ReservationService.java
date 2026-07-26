d966533ce8744f74f6d89ea888da393f3b631afb
Sergio <sergiorod210706@gmail.com>
2026-07-26 13:35:05 -0600

    feat: auto assign available guide on reservation creation

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/services/ReservationService.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/services/ReservationService.java
@@ -27,88 +27,84 @@ public class ReservationService {
     private final GuideRepository guideRepository;
     private final TravelPackageRepository travelPackageRepository;
 
-    public ReservationService(ReservationRepository reservationRepository, UserRepository userRepository, GuideRepository guideRepository, TravelPackageRepository travelPackageRepository){
+    public ReservationService(ReservationRepository reservationRepository, UserRepository userRepository,
+            GuideRepository guideRepository, TravelPackageRepository travelPackageRepository) {
         this.reservationRepository = reservationRepository;
         this.userRepository = userRepository;
         this.guideRepository = guideRepository;
         this.travelPackageRepository = travelPackageRepository;
     }
 
-    private ReservationResponseDTO toResponseDTO(Reservation reservation){
+    private ReservationResponseDTO toResponseDTO(Reservation reservation) {
         return new ReservationResponseDTO(
-            reservation.getId(), 
-            reservation.getReservationDate(), 
-            reservation.getNumberOfPeople(), 
-            reservation.getReservationStatus(), 
-            reservation.getCustomer().getId(), 
-            reservation.getCustomer().getFirstName().concat(" ").concat(reservation.getCustomer().getLastName()), 
-            reservation.getTravelPackage().getId(), 
-            reservation.getTravelPackage().getPackageName(), 
-            reservation.getGuide().getId(),
-            reservation.getGuide().getFirstName().concat(" ").concat(reservation.getGuide().getLastName())
-        );
+                reservation.getId(),
+                reservation.getReservationDate(),
+                reservation.getNumberOfPeople(),
+                reservation.getReservationStatus(),
+                reservation.getCustomer().getId(),
+                reservation.getCustomer().getFirstName().concat(" ").concat(reservation.getCustomer().getLastName()),
+                reservation.getTravelPackage().getId(),
+                reservation.getTravelPackage().getPackageName(),
+                reservation.getGuide().getId(),
+                reservation.getGuide().getFirstName().concat(" ").concat(reservation.getGuide().getLastName()));
     }
 
-    private Reservation findById(Long id){
-        return reservationRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Reservation not found"));
+    private Reservation findById(Long id) {
+        return reservationRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Reservation not found"));
     }
 
-    public List<ReservationResponseDTO> getAllReservations(){
+    public List<ReservationResponseDTO> getAllReservations() {
         return reservationRepository.findAll()
-            .stream()
-            .map(this::toResponseDTO)
-            .collect(Collectors.toList());
+                .stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
     }
 
-    public ReservationResponseDTO getReservationById(Long id){
+    public ReservationResponseDTO getReservationById(Long id) {
         return toResponseDTO(findById(id));
     }
 
-    public ReservationResponseDTO createReservation(ReservationRequestDTO requestDTO, String email){
+    public ReservationResponseDTO createReservation(ReservationRequestDTO requestDTO, String email) {
         LocalDate date = LocalDate.now();
-        User user = userRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("User not found"));
-        Guide guide = guideRepository.findById(requestDTO.getGuideId()).orElseThrow(()-> new ResourceNotFoundException("Guide not found"));
-        TravelPackage travelPackage = travelPackageRepository.findById(requestDTO.getTravelPackageId()).orElseThrow(()-> new ResourceNotFoundException("TravelPackage not found"));
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        TravelPackage travelPackage = travelPackageRepository.findById(requestDTO.getTravelPackageId())
+                .orElseThrow(() -> new ResourceNotFoundException("TravelPackage not found"));
+
+        Guide guide = guideRepository.findByAvailable(true)
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("No guides availables at the moment"));
 
         guide.setAvailable(false);
         guideRepository.save(guide);
 
-        Reservation reservation = new Reservation(date, requestDTO.getNumberOfPeople(), ReservationStatus.PENDING,user, travelPackage, guide);
+        Reservation reservation = new Reservation(date, requestDTO.getNumberOfPeople(), ReservationStatus.PENDING, user,
+                travelPackage, guide);
 
         reservationRepository.save(reservation);
         return toResponseDTO(reservation);
     }
 
-
-    public ReservationResponseDTO updateReservation(Long id, ReservationRequestDTO requestDTO){
+    public ReservationResponseDTO updateReservation(Long id, ReservationRequestDTO requestDTO) {
         var reservation = findById(id);
 
-        Guide assignedGuide = guideRepository.findById(reservation.getGuide().getId()).orElseThrow(()-> new ResourceNotFoundException("Guide not found"));
-
-        Guide guide = guideRepository.findById(requestDTO.getGuideId()).orElseThrow(()-> new ResourceNotFoundException("Guide not found"));
-
-        TravelPackage travelPackage = travelPackageRepository.findById(requestDTO.getTravelPackageId()).orElseThrow(()-> new ResourceNotFoundException("TravelPackage not found"));
-
-        if (!assignedGuide.getId().equals(guide.getId())){
-            assignedGuide.setAvailable(true);
-            guideRepository.save(assignedGuide);
-            guide.setAvailable(false);
-            guideRepository.save(guide);
-        }
+        TravelPackage travelPackage = travelPackageRepository.findById(requestDTO.getTravelPackageId())
+                .orElseThrow(() -> new ResourceNotFoundException("TravelPackage not found"));
 
         reservation.setNumberOfPeople(requestDTO.getNumberOfPeople());
-        reservation.setGuide(guide);
         reservation.setTravelPackage(travelPackage);
-        
+
         reservationRepository.save(reservation);
         return toResponseDTO(reservation);
     }
 
-    public void deleteReservation(Long id){
+    public void deleteReservation(Long id) {
         reservationRepository.delete(findById(id));
     }
 
-    public ReservationResponseDTO confirmReservation(Long id){
+    public ReservationResponseDTO confirmReservation(Long id) {
         var reservation = findById(id);
         reservation.setReservationStatus(ReservationStatus.CONFIRMED);
         reservationRepository.save(reservation);
@@ -116,35 +112,32 @@ public class ReservationService {
         return toResponseDTO(reservation);
     }
 
-    public ReservationResponseDTO cancelReservation(Long id){
+    public ReservationResponseDTO cancelReservation(Long id) {
         var reservation = findById(id);
         reservation.setReservationStatus(ReservationStatus.CANCELED);
         reservationRepository.save(reservation);
 
         reservation.getGuide().setAvailable(true);
         guideRepository.save(reservation.getGuide());
-        
+
         return toResponseDTO(reservation);
     }
 
-    
-    public List<ReservationResponseDTO> getUserReservations(Long id){
+    public List<ReservationResponseDTO> getUserReservations(Long id) {
         if (reservationRepository.findByUserId(id).isEmpty()) {
             throw new ResourceNotFoundException("No reservations");
         }
         return reservationRepository.findByUserId(id).stream()
-        .map(this::toResponseDTO).collect(Collectors.toList());
+                .map(this::toResponseDTO).collect(Collectors.toList());
     }
 
-    public List<ReservationResponseDTO> getMyReservations(String email){
-        var user = userRepository.findByEmail(email).orElseThrow(()-> new ResourceNotFoundException("User not found"));
+    public List<ReservationResponseDTO> getMyReservations(String email) {
+        var user = userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         return reservationRepository.findByUserId(user.getId())
-        .stream()
-        .map(this :: toResponseDTO)
-        .collect(Collectors.toList());
+                .stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
     }
-
-
 
 }
