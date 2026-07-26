640da41d4853a7bcf38c1b8f1dbcf56729f78dc0
Sergio <sergiorod210706@gmail.com>
2026-07-26 13:35:05 -0600

    refactor: remove guideId from ReservationRequestDTO

--- backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/models/dto/request/ReservationRequestDTO.java
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/models/dto/request/ReservationRequestDTO.java
@@ -1,14 +1,11 @@
 package com.utma.tams.travel_agency_management_system_api.models.dto.request;
 
-import java.time.LocalDate;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class ReservationRequestDTO {
 
-    @NotNull(message = "Reservation date is mandatory.")
-    private LocalDate reservationDate;
 
     @NotNull(message = "Number of people is mandatory.")
     @Min(value = 1, message = "Number of people must be at least 1.")
@@ -17,26 +14,15 @@ public class ReservationRequestDTO {
     @NotNull(message = "Travel package is mandatory.")
     private Long travelPackageId;
 
-    @NotNull(message = "Guide is mandatory.")
-    private Long guideId;
 
-    public ReservationRequestDTO(LocalDate reservationDate, Integer numberOfPeople, Long packageId, Long guideId){
-        this.reservationDate = reservationDate;
+    public ReservationRequestDTO(Integer numberOfPeople, Long packageId, Long guideId){
         this.numberOfPeople = numberOfPeople;
         this.travelPackageId = packageId;
-        this.guideId = guideId;
     }
 
     public ReservationRequestDTO() {
     }
 
-    public LocalDate getReservationDate() {
-        return reservationDate;
-    }
-
-    public void setReservationDate(LocalDate reservationDate) {
-        this.reservationDate = reservationDate;
-    }
 
     public Integer getNumberOfPeople() {
         return numberOfPeople;
@@ -53,15 +39,5 @@ public class ReservationRequestDTO {
     public void setTravelPackageId(Long travelPackageId) {
         this.travelPackageId = travelPackageId;
     }
-
-    public Long getGuideId() {
-        return guideId;
-    }
-
-    public void setGuideId(Long guideId) {
-        this.guideId = guideId;
-    }
-
-    
 
 }
