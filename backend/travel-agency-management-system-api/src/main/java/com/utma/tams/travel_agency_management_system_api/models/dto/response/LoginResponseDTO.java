3b4cee4f96c6702e3a7713d98f9d7704c79d5595
SergioRdzMndz <utm24090656@utma.edu.mx>
2026-07-26 00:41:20 -0600

    feat: add LoginResponseDTO

---
+++ backend/travel-agency-management-system-api/src/main/java/com/utma/tams/travel_agency_management_system_api/models/dto/response/LoginResponseDTO.java
@@ -0,0 +1,39 @@
+package com.utma.tams.travel_agency_management_system_api.models.dto.response;
+
+public class LoginResponseDTO {
+    private String token;
+    private String email;
+    private String role;
+
+    public LoginResponseDTO(String token, String email, String role) {
+        this.token = token;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+}
